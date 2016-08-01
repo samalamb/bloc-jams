181e090656d73ff63b1e72c7f4cc4b2ead6fb472
Sam Dieck <samuel.dieck@gmail.com>
2016-08-01 14:11:03 -0700

    Checkpoint 23 work completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-var animatePoints = function(){
-    var points = document.getElementsByClassName('point');
 
-    var revealPoint = function(index){
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
+    var revealPoints = function(index){
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translate(0)";
@@ -9,7 +10,22 @@ var animatePoints = function(){
     };
     
     for(var i = 0; i < points.length; i++){
-        revealPoint(i);
+        revealPoints(i);
     }
     
+};
+
+window.onload = function(){
+    
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event){
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+             animatePoints(pointsArray);   
+         }
+    });
 };
