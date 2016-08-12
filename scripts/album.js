8fe3779590c10dd4eae5434e635862591bece0d8
Sam Dieck <samuel.dieck@gmail.com>
2016-08-12 09:19:06 -0700

    Final foundation assignment completed

--- scripts/album.js
+++ scripts/album.js
@@ -11,6 +11,24 @@ var setSong = function(songNumber){
     setVolume(currentVolume);
 };
 
+var filterTimeCode = function(timeInSeconds){
+    minuteTime = Math.floor(timeInSeconds / 60);
+    secondTime = Math.floor(parseFloat(timeInSeconds % 60));
+    
+    if(secondTime < 10){
+        secondTime = '0' + secondTime;
+    }
+    return minuteTime + ':' + secondTime;
+};
+
+var setCurrentTimeInPlayerBar = function(currentTime){
+    $('.current-time').text(currentTime);
+};
+
+var setTotalTimeInPlayerBar = function(totalTime){
+    $('.total-time').text(totalTime);
+};
+
 var seek = function(time){
     if(currentSoundFile){
         currentSoundFile.setTime(time);
@@ -32,7 +50,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         '<tr class="album-view-song-item">'
     +   '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     +   '  <td class="song-item-title">' + songName + '</td>'
-    +   '  <td class="song-item-duration">' + songLength + '</td>'
+    +   '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
     +   '</tr>'
     ;
 
@@ -121,7 +139,7 @@ var updateSeekBarWhileSongPlays = function() {
         currentSoundFile.bind('timeupdate', function(event) {
             var seekBarFillRatio = this.getTime() / this.getDuration();
             var $seekBar = $('.seek-control .seek-bar');
-
+            setCurrentTimeInPlayerBar(filterTimeCode(this.getTime()));
             updateSeekPercentage($seekBar, seekBarFillRatio);
      });
     }
@@ -243,6 +261,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.duration));
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
